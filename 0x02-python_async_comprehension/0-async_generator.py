c9e3e856d4745e4748df469e8a37ce33a17e3e4b
rosyrose1 <rosinaantwi96@gmail.com>
2024-02-20 08:02:07 -0800

    async generator that Loop 10 times, wait 1 sec each time

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -15,4 +15,4 @@ async def async_generator() -> Generator[float, None, None]:
     """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.random(0) * 10
